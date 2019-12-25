c84937a918cd9fd9f0a76e40136754227f14c1fa
Alex Cruz <du.alexcruz@gmail.com>
2019-12-24 20:41:51 -0500

    Resolve theme issues (see commit message)
    - Darken up the resolver background
    - Fixed QS notifications background
    - Fixed QS panel and customizer background
    - Fixed lockscreen notification background
    - Fixed Settings 'sensors off' drawable under Developer options
    - Darken up the Settings/QS/theme background to match GAPPS
    - Darken up volume panel dialog and other dialogs
    - Fixed screen record dialog, text and background
    
    Change-Id: Ib94c8064ed340092cd90f0bfa0b3897e10d4c25d

--- themes.mk
+++ themes.mk
@@ -36,3 +36,9 @@ PRODUCT_PACKAGES += \
     UserSeven \
     Yellow \
     White
+
+# Add theme specific overlays
+DEVICE_PACKAGE_OVERLAYS += vendor/themes/overlay/common
+
+# Allow overlays to be excluded from enforcing RRO
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/themes/overlay/common
