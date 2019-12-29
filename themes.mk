d23fcbcc0c5abfaba6d47d924f66fea154adc20d
Pranav Vashi <neobuddy89@gmail.com>
2019-12-29 17:27:38 +0530

    Add our favourite fonts in AOSP FontEngine [1/2]
    Change-Id: I8b1bd434beab9bd53a82484c9fd0a2274d313b5d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- themes.mk
+++ themes.mk
@@ -35,6 +35,18 @@ PRODUCT_PACKAGES += \
     UserSeven \
     Yellow
 
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/themes/prebuilt/system/fonts/Aclonica.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Aclonica.ttf \
+    vendor/themes/prebuilt/system/fonts/Amarante.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Amarante.ttf \
+    vendor/themes/prebuilt/system/fonts/Bariol-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Bariol-Regular.ttf \
+    vendor/themes/prebuilt/system/fonts/Cagliostro-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Cagliostro-Regular.ttf \
+    vendor/themes/prebuilt/system/fonts/Coolstory-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Coolstory-Regular.ttf \
+    vendor/themes/prebuilt/system/fonts/LGSmartGothic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/LGSmartGothic.ttf \
+    vendor/themes/prebuilt/system/fonts/Rosemary-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Rosemary-Regular.ttf \
+    vendor/themes/prebuilt/system/fonts/SonySketch.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/SonySketch.ttf \
+    vendor/themes/prebuilt/system/fonts/Surfer.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Surfer.ttf
+
 # Add theme specific overlays
 DEVICE_PACKAGE_OVERLAYS += vendor/themes/overlay/common
 
