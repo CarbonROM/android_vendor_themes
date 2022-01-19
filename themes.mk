f103c7cbb43a1b1e90b3df5b53d6f6ac42ae18bb
Michael Bestas <mkbestas@gmail.com>
2022-01-20 00:16:05 +0200

    Import Android 11 icon shapes
    Google removed them from frameworks/base, we relocated them here
    and build them again.
    
    Change-Id: Icd83b1d5b4cc6e8c679a7e348618ee2b42517073

--- themes.mk
+++ themes.mk
@@ -8,3 +8,13 @@ DEVICE_PACKAGE_OVERLAYS += vendor/themes/overlay/common
 
 # Allow overlays to be excluded from enforcing RRO
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/themes/overlay/common
+
+# IconShapes
+PRODUCT_PACKAGES += \
+    IconShapePebbleOverlay \
+    IconShapeRoundedRectOverlay \
+    IconShapeSquareOverlay \
+    IconShapeSquircleOverlay \
+    IconShapeTaperedRectOverlay \
+    IconShapeTeardropOverlay \
+    IconShapeVesselOverlay
