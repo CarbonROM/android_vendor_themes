66681c101173fb1a2c1434817b9d7dcbaf196dc9
Christian Oder <myself5@carbonrom.org>
2022-12-10 13:57:14 +0100

    themes: Move overlays to vendor/carbon
    * Remove unused overlays and move remaining overlays to vendor/carbon
    
    Change-Id: If9198432b02fcdb43af6ddcca878c768d51e3df7

--- themes.mk
+++ themes.mk
@@ -3,12 +3,6 @@ PRODUCT_PACKAGES += \
     CarbonBlackTheme \
     CarbonThemesStub
 
-# Add theme specific overlays
-DEVICE_PACKAGE_OVERLAYS += vendor/themes/overlay/common
-
-# Allow overlays to be excluded from enforcing RRO
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/themes/overlay/common
-
 # IconShapes
 PRODUCT_PACKAGES += \
     IconShapePebbleOverlay \
